00d235cbb30bc028c732df55375d4c8f94c431e3
jhimy-michel <jhimy0riv@gmail.com>
2023-07-19 20:32:28 +0200

    Initalizing project

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+const options = {
+  arrowPattern: "avoid",
+  singleQuote: true,
+  bracketSpacing: true,
+  endOfLine: "lf",
+  semi: false,
+  tabWidth: 2,
+  trailingComma: "none",
+};
+
+module.exports = options;
