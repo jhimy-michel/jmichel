2e30afe4d47f2fbc30fd0f0cd9b1123d164c32b7
JMichel <jhimy0riv@gmail.com>
2023-10-24 12:59:28 +0200

    fixing index page

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import { Container, Box } from '@chakra-ui/react'
+
+const Home = () => {
+  return (
+    <Container>
+      <Box borderRadius="lg">
+        Hello, I am a full-stack developer based in Switzerland!
+      </Box>
+    </Container>
+  )
+}
+
+export default Home
