44d49b602039bc69b973a4bb60ab0b5e8ae33195
JMichel <jhimy0riv@gmail.com>
2023-10-25 13:55:14 +0200

    adding sections and fonts

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
-import { Container, Box, Heading } from '@chakra-ui/react'
+import {
+  Container,
+  Box,
+  Heading,
+  Image,
+  useColorModeValue
+} from '@chakra-ui/react'
+import Section from '../components/section'
 
 const Home = () => {
   return (
     <Container>
-      <Box borderRadius="lg" bg="red" p={3} align="center" mb={6}>
+      <Box
+        borderRadius="lg"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+        p={3}
+        align="center"
+        mb={6}
+      >
         Hello, I&apos;m a full-stack developer based in Switzerland!
       </Box>
       <Box display={{ md: 'flex' }}>
@@ -13,7 +26,31 @@ const Home = () => {
           </Heading>
           <p>Digital Cratfzman (Artist / Developer/ )</p>
         </Box>
+
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          align="center"
+        >
+          <Image
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            maxWidth="100px"
+            display="inline-block"
+            borderRadius="full"
+            alt="profile image"
+            src="/images/takuya.jpg"
+          />
+        </Box>
       </Box>
+      <Section delay={0.1}>
+        <Heading as="h3" variant="section-title">
+          Work
+        </Heading>
+        <p>Paragraph</p>
+      </Section>
     </Container>
   )
 }
