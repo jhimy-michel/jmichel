0c76a0f24f2592fd768877e77c996c703ce79f02
JMichel <jhimy0riv@gmail.com>
2023-10-25 21:11:40 +0200

    adding validation for production path

--- components/voxel-dog.js
+++ components/voxel-dog.js
@@ -12,7 +12,8 @@ const VoxelDog = () => {
   const refContainer = useRef()
   const [loading, setLoading] = useState(true)
   const refRenderer = useRef()
-  const urlDogGLB = '/perrito4.glb'
+  const isProd = process.env.NODE_ENV === 'production'
+  const urlDogGLB = isProd ? '/jmichel/perrito4.glb' : '/perrito4.glb'
 
   const handleWindowResize = useCallback(() => {
     const { current: renderer } = refRenderer
--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
   reactStrictMode: true,
-  basePath: '/jmichel',
+  basePath: isProd ? '/jmichel' : ''
 }
