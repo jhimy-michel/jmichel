a1df30e6aeb8cf85d18834c717d27a21c7a0fb66
JMichel <jhimy0riv@gmail.com>
2023-10-26 12:40:10 +0200

    fixing assets url

--- next.config.js
+++ next.config.js
@@ -2,5 +2,6 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   reactStrictMode: true,
-  basePath: isProd ? '/jmichel' : ''
+  basePath: isProd ? '/jmichel' : '',
+  assetPrefix: !isProd ? 'https://jhimy-michel.github.io/jmichel/' : '',
 }
