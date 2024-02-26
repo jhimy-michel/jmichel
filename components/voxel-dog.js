2ccd5759eabbf2d29745d0015a046ec5a14be426
jhimy-michel <jhimy0riv@gmail.com>
2024-02-26 14:00:12 +0100

    updating dog

--- components/voxel-dog.js
+++ components/voxel-dog.js
@@ -12,7 +12,7 @@ const VoxelDog = () => {
   const refContainer = useRef()
   const [loading, setLoading] = useState(true)
   const refRenderer = useRef()
-  const urlDogGLB = '/perritoMatecito.glb'
+  const urlDogGLB = '/perrito.glb'
 
   const handleWindowResize = useCallback(() => {
     const { current: renderer } = refRenderer
